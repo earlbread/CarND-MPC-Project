c2072ccab0fba4e8b442afce0eefce0246158453
Seunghun Lee <waydi1@gmail.com>
2017-07-23 10:40:49 +0900

    Decrease delta difference in cost function

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -59,7 +59,7 @@ class FG_eval {
     }
 
     for (int t = 0; t < N - 2; t++) {
-      fg[0] += 500 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 100 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
